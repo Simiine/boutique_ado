d2b9fa6314f42d254028bf841b2d0300502801c6
Simi <simiinemboj@gmail.com>
2022-12-13 19:21:32 +0000

    Added stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MEdJRBpYmvCMimZd39imxdzIXsoD1Ntik0KDPSHC7scQehelOU0xgQdeZTRmAdh0slfMFuVTwSWduvf1WsmGIBh00dDDVl0IE',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
